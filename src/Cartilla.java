517df433ae6958b7c1e8f6627926cbf45e36be77
zubiri <irecaldema14dw@ikzubirimanteo.com>
2014-12-17 12:52:05 +0000

    3.15

---
+++ src/Cartilla.java
@@ -0,0 +1,7 @@
+class Cartilla {
+private boolean[] sellos; //array de 10, para guardar los sellos, cada, item sera un bar
+
+//metodo sellar
+//metodo devolver dto correspondiente
+
+}
---
+++ src/Pote.java
@@ -0,0 +1,9 @@
+class Pote {
+private int Id;
+private String Nombre;
+private int Tipo; //1: Basico 2: Selecto
+
+//1: Agua, zurito, vino del año
+//2: Caña, refrescos, vino crianza
+
+}
